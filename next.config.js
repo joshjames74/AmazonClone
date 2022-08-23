e3ab11bacaabbea294e10de61c37237250715978
Joshua <joshua.tomos.james@gmail.com>
2022-08-23 14:07:53 +0100

    Initialize git repo

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false};
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
